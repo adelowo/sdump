6bc4f170efce746ba292aa2468879a0ea8c4a7b5
Lanre Adelowo <yo@lanre.wtf>
2024-01-15 02:58:26 +0100

    Setup configuration (#1)
    * basic configuration setup
    
    * add GH actions

---
+++ cmd/cmd.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/adelowo/sdump/config"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	// Version describes the version of the current build.
+	Version = "dev"
+
+	// Commit describes the commit of the current build.
+	Commit = "none"
+
+	// Date describes the date of the current build.
+	Date = time.Now().UTC()
+)
+
+const (
+	defaultConfigFilePath = "config"
+	envPrefix             = "SDUMP"
+)
+
+func initializeConfig(cfg *config.Config) error {
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	viper.AddConfigPath(filepath.Join(homePath, ".config", defaultConfigFilePath))
+	viper.AddConfigPath(".")
+
+	viper.SetConfigName(defaultConfigFilePath)
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	viper.SetEnvPrefix(envPrefix)
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	return viper.Unmarshal(cfg)
+}
+
+func setupFlags(cmd *cobra.Command) {
+	cmd.Flags().StringP("config", "c", defaultConfigFilePath, "Config file")
+}
+
+func Execute() error {
+	cfg := &config.Config{}
+
+	rootCmd := &cobra.Command{
+		Use:   "sdump",
+		Short: "sdump runs a SSH server that helps you view and inspect incoming http requests",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return initializeConfig(cfg)
+		},
+		RunE: func(_ *cobra.Command, _ []string) error {
+			return nil
+		},
+	}
+
+	rootCmd.SetVersionTemplate(
+		fmt.Sprintf("Version: %v\nCommit: %v\nDate: %v\n", Version, Commit, Date))
+
+	setupFlags(rootCmd)
+
+	return rootCmd.Execute()
+}
