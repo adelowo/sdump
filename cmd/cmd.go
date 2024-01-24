3cbcbb7a81680e51d9e2aa31cc290be939a6dedf
Lanre Adelowo <yo@lanre.wtf>
2024-01-24 14:39:59 +0100

    limit http request body size (#12)

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -63,6 +63,7 @@ func initializeConfig(cfg *config.Config) error {
 	viper.SetDefault("http.database.log_queries", false)
 	viper.SetDefault("http.port", 4200)
 	viper.SetDefault("http.domain", "sdump.app")
+	viper.SetDefault("http.max_request_body", 1024)
 
 	return viper.Unmarshal(cfg)
 }
