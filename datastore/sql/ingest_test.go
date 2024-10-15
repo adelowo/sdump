7964947fdedd7d30eb72c47d0b237d75fec7d136
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-10-15 17:54:19 +0100

    remove integration test for sqlite

--- datastore/sql/ingest_test.go
+++ datastore/sql/ingest_test.go
@@ -31,27 +31,3 @@ func TestIngestRepository_Create(t *testing.T) {
 		},
 	}))
 }
-
-func TestIngestRepository_Sqllite_Create(t *testing.T) {
-	client, teardownFunc := setupSqliteDatabase(t)
-	defer teardownFunc()
-
-	ingestStore := NewIngestRepository(client)
-
-	urlStore := NewURLRepositoryTable(client)
-
-	// the models use uuid, tricky bit now is setting up sqlite3 uuid support
-	// This currently fails as the migrations fail
-
-	endpoint, err := urlStore.Get(context.Background(), &sdump.FindURLOptions{
-		Reference: "cmltfm6g330l5l1vq110", // see fixtures/urls.yml
-	})
-	require.NoError(t, err)
-
-	require.NoError(t, ingestStore.Create(context.Background(), &sdump.IngestHTTPRequest{
-		UrlID: endpoint.ID,
-		Request: sdump.RequestDefinition{
-			Body: "{}",
-		},
-	}))
-}
